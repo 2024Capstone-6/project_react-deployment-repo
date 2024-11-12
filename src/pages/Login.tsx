77d527f950a44d43bd2122ccca7677ccfff5ab18
yjkdev <dustmqgksmsdev24@gmail.com>
2024-11-12 19:09:23 +0900

    ✨ feat: 컨테이너 생성

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,15 @@
 const Login: React.FC = () => {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="login-container" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <div className="input-field">
+        <input type="text" placeholder="Email" />
+      </div>
+      <div className="input-field">
+        <input type="password" placeholder="Password" />
+      </div>
+      <div className="sign-up-text">
+        <p>New to User? <a style={{ color: 'blue' }} href="/signup">Sign Up</a></p>
+      </div>
     </div>
   );
 };
