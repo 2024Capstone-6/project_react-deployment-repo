8489cafa45627602f3feaa7968c31858309fc117
EUN YEONG MIN <qlafqjsgh123@naver.com>
2024-11-13 19:54:50 +0900

    ✨ feat: 기본 틀 잡기
    기본 틀 잡기

--- src/pages/Board.tsx
+++ src/pages/Board.tsx
@@ -1,11 +1,21 @@
+import { useNavigate } from "react-router-dom";
 import "../style/board.css"
 
 const Board: React.FC = () => {
+  const navigate = useNavigate();
   return (
-    <div>
-      <h1>Board</h1>
-      <p>허허</p>
+    <>
+    <div className="mainpage">
+      글목록~
+      <div className="createPost" onClick={()=> navigate('/createPost')}>글쓰기</div>
+      <div className="boardbox">임마는 글박스 디자인
+        
+      </div>
     </div>
+    <div className="bestpage">
+      베스트글~
+    </div>
+    </>
   );
 };
 
