02b66bfbc9d8a76a39e81d8cac0b567d71815539
alswns0340 <alswns5870@naver.com>
2024-11-20 20:58:35 +0900

    ✨ feat: spcial random 문제 및 답 입력

--- src/pages/Special/Special.tsx
+++ src/pages/Special/Special.tsx
@@ -1,31 +1,52 @@
 import axios from 'axios'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { children } from 'solid-js'
 
 
 const Special = () => {
+  const [Question,setQuestion] = useState('')
+  const [answer,setAnswer] = useState('')
+  useEffect(()=>{
+    test() 
+  },[])
 
-  function checkAnswer(answer : string){
-    const question = axios.get('localhost:')
+  const  test = async ()=>{
+    const testarr=await axios.get("http://localhost:3001/special")
+    const len:number = testarr.data.length // 안에있는 데이터의 길이
+    const randomNum = Math.floor(Math.random()*len+testarr.data[0].id)
+    setQuestion(testarr.data[randomNum-1].Question)
+    setAnswer(testarr.data[randomNum-1].answer)
+  }
+
+  const checkAnswer = (c:any)=>{
+    if(answer===c){
+      console.log('정답.')
+    }
+    else{
+      console.log(`틀림 ${c} 병신아`)
+    }
   }
 
   return (
     <div>
-      <div className='Question'>문제 박스</div>
+      <div className='Question'>{Question}</div>
       <form onSubmit={(e)=>{
         e.preventDefault()
         const answer = e.currentTarget.answer.value // input창의 값 읽어옴.
-        console.log(answer)
+        checkAnswer(answer)
 
       }}
       >
         <input style={{
           border:'solid 1px black',
-          borderRadius:'10px'
+          borderRadius:'10px',
+          paddingLeft:'1%'
         }}
         name='answer'
         />
-        <button type='submit' style={{border:'solid 1px black', marginLeft:'.5%'}}>제출</button>
+        <button type='submit' style={{border:'solid 1px black', marginLeft:'.5%' }}>제출</button>
       </form>
+      <button onClick={test}>test</button>
     </div>
   )
 }
